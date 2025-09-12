1721ed448dc5b2cde21f2732f168fbfb24df4a6e
Yaseen Kola <yaseenkola@gmail.com>
2025-09-12 22:35:18 +0400

    added intro_tables_assignments files

---
+++ intro and tables/intro.js
@@ -0,0 +1,8 @@
+let x = 10; // x is a variable (container)
+let name = "Yaseen";
+const pi = 3.142;
+
+let y = x + 5;
+
+console.log("x value is " + x);
+console.log("y value is " + y);
---
+++ intro and tables/tables.js
@@ -0,0 +1,5 @@
+let n = 5;
+for (let i = 1; i <= 10; i++) {
+  let result = n * i;
+  console.log(n + " x " + i + " = " + result);
+}
